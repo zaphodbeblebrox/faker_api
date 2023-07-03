5323cb8ae048437ebbba819de759f91f86d5e315
Brandon Withrow <withrobm@gmail.com>
2023-07-02 20:00:59 -0700

    updated for the faker project
    NOTE: I cannot get Faker to properly work on my pc for some reason, so this code is best guess

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 const express = require("express");
+const faker = require("@faker-js/faker");
+
 const app = express();
 const port = 8000;
 
+const createUser = () => {
+    const newFake = {
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.phoneNumber(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        _id: faker.random.uuid()
+    };
+    return newFake;
+};
+
+const createCompany = () => {
+    const newFake = {
+        _id: faker.random.uuid(),
+        name: faker.company.companyName(),
+        address: {
+            street: faker.address.streetName(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country()
+        }
+    };
+    return newFake;
+};
+
+app.get("/api/users/new", (req, res) => {
+    res.json({user: createUser});
+});
+
+app.get("/api/companies/new", (req, res) => {
+    res.json({company: createCompany});
+});
+
+app.get("/api/user/company", (req, res) => {
+    res.json({user:createUser, company:createCompany});
+});
+    
 const apiRoutes = require("./routes/apiRoutes");
 apiRoutes(app);
 
